c9fc852da7224dc9d06206911de273bca1c1d114
MugenAttack <MugenAttack@ymail.com>
2018-03-19 19:46:52 -0500

    Revert "updated MSG Editor and list generator csv"
    This reverts commit 639a21518dc8d98dd86567d23543d36c99ced262.

--- XV2Lib/CST.cs
+++ XV2Lib/CST.cs
@@ -6,28 +6,7 @@ using System.Threading.Tasks;
 
 namespace XV2Lib
 {
-    struct CST_Slot
-    {
-
-    }
-
-    struct Sub_Slot
-    {
-        string shortname;
-        int unk1;
-        int unk2;
-
-    }
-
     class CST
     {
-        public List<CST_Slot> Read(string filename)
-        {
-            List<CST_Slot> slots = new List<CST_Slot>();
-
-
-
-            return slots;
-        }
     }
 }
--- XV2ListGeneratorCSV/Program.cs
+++ XV2ListGeneratorCSV/Program.cs
@@ -11,9 +11,6 @@ namespace XV2ListGeneratorCSV
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter in Separator: ");
-            string sep = Console.ReadLine();
-
             skill[] Super = new skill[1];
             skill[] Ultimate = new skill[1];
             skill[] Evasive = new skill[1];
@@ -22,96 +19,60 @@ namespace XV2ListGeneratorCSV
             Settings s = new Settings();
             s.Read();
 
-            CUS.ReadSkills(s.XENOFolder + "data/system" + "/custom_skill.cus", ref Super, ref Ultimate, ref Evasive, ref Awaken, ref blast);
+            CUS.ReadSkills(s.SysFolder + "/custom_skill.cus", ref Super, ref Ultimate, ref Evasive, ref Awaken, ref blast);
 
-            MSG m = MSGStream.Read(s.XENOFolder + "data/msg" + "/proper_noun_skill_spa_name_" + s.language + ".msg");
+            MSG m = MSGStream.Read(s.MSGFolder + "/proper_noun_skill_spa_name_" + s.language + ".msg");
 
             StreamWriter sw = new StreamWriter("Super_" + s.language + ".csv");
-            sw.WriteLine("ID (Numeric)" + sep + "ID (HEX)" + sep + "Short Name" + sep + "Full Name");
+            sw.WriteLine("ID (Numeric),ID (HEX),Short Name,Full Name");
             foreach (skill sk in Super)
             {
                 string txt = m.Find("spe_skill_" + sk.id2.ToString("0000"));
-                sw.WriteLine(sk.id.ToString() + sep + ReverseHexString4(sk.id.ToString("x4")) + sep + sk.shortName + sep + txt);
+                sw.WriteLine(sk.id.ToString() + "," + ReverseHexString4(sk.id.ToString("x4")) + "," + sk.shortName + "," + txt);
             }
 
             sw.Close();
 
-            m = MSGStream.Read(s.XENOFolder + "data/msg" + "/proper_noun_skill_ult_name_" + s.language + ".msg");
+            m = MSGStream.Read(s.MSGFolder + "/proper_noun_skill_ult_name_" + s.language + ".msg");
             sw = new StreamWriter("Ultimate_" + s.language + ".csv");
-            sw.WriteLine("ID (Numeric)" + sep + "ID (HEX)" + sep + "Short Name" + sep + "Full Name");
+            sw.WriteLine("ID (Numeric),ID (HEX),Short Name,Full Name");
             foreach (skill sk in Ultimate)
             {
                 string txt = m.Find("ult_" + sk.id2.ToString("0000"));
-                sw.WriteLine(sk.id.ToString() + sep + ReverseHexString4(sk.id.ToString("x4")) + sep + sk.shortName + sep + txt);
+                sw.WriteLine(sk.id.ToString() + "," + ReverseHexString4(sk.id.ToString("x4")) + "," + sk.shortName + "," + txt);
             }
 
             sw.Close();
 
-            m = MSGStream.Read(s.XENOFolder + "data/msg" + "/proper_noun_skill_esc_name_" + s.language + ".msg");
+            m = MSGStream.Read(s.MSGFolder + "/proper_noun_skill_esc_name_" + s.language + ".msg");
             sw = new StreamWriter("Evasive_" + s.language + ".csv");
-            sw.WriteLine("ID (Numeric)" + sep + "ID (HEX)" + sep + "Short Name" + sep + "Full Name");
+            sw.WriteLine("ID (Numeric),ID (HEX),Short Name,Full Name");
             foreach (skill sk in Evasive)
             {
                 string txt = m.Find("avoid_skill_" + sk.id2.ToString("0000"));
-                sw.WriteLine(sk.id.ToString() + sep + ReverseHexString4(sk.id.ToString("x4")) + sep + sk.shortName + sep + txt);
+                sw.WriteLine(sk.id.ToString() + "," + ReverseHexString4(sk.id.ToString("x4")) + "," + sk.shortName + "," + txt);
             }
 
             sw.Close();
 
-            m = MSGStream.Read(s.XENOFolder + "data/msg" + "/proper_noun_skill_met_name_" + s.language + ".msg");
+            m = MSGStream.Read(s.MSGFolder + "/proper_noun_skill_met_name_" + s.language + ".msg");
             sw = new StreamWriter("Awaken_" + s.language + ".csv");
-            sw.WriteLine("ID (Numeric)" + sep + "ID (HEX)" + sep + "Short Name" + sep + "Full Name");
+            sw.WriteLine("ID (Numeric),ID (HEX),Short Name,Full Name");
             foreach (skill sk in Awaken)
             {
                 string txt = m.Find("met_skill_" + sk.id2.ToString("0000"));
-                sw.WriteLine(sk.id.ToString() + sep + ReverseHexString4(sk.id.ToString("x4")) + sep + sk.shortName + sep + txt);
+                sw.WriteLine(sk.id.ToString() + "," + ReverseHexString4(sk.id.ToString("x4")) + "," + sk.shortName + "," + txt);
             }
             sw.Close();
 
-            m = MSGStream.Read(s.XENOFolder + "data/msg" + "/proper_noun_character_name_" + s.language + ".msg");
-            Char_Model_Spec[] cms = CMS.Read(s.XENOFolder + "data/system" + "/char_model_spec.cms");
+            m = MSGStream.Read(s.MSGFolder + "/proper_noun_character_name_" + s.language + ".msg");
+            Char_Model_Spec[] cms = CMS.Read(s.SysFolder + "/char_model_spec.cms");
             sw = new StreamWriter("Character_" + s.language + ".csv");
-            sw.WriteLine("ID (Numeric)" + sep + "ID (HEX)" + sep + "Short Name" + sep + "Full Name");
+            sw.WriteLine("ID (Numeric),ID (HEX),Short Name,Full Name");
             foreach (Char_Model_Spec c in cms)
             {
                 string txt = m.Find("chara_" + c.shortname + "_000");
-                sw.WriteLine(c.id.ToString() + sep + ReverseHexString4(c.id.ToString("x4")) + sep + c.shortname + sep + txt);
-            }
-            sw.Close();
-
-            m = MSGStream.Read(s.XENOFolder + "data/msg" + "/proper_noun_talisman_name_" + s.language + ".msg");
-            IDB idb = new IDB();
-            idb.Read(s.XENOFolder + "data/system/item/talisman_item.idb");
-            sw = new StreamWriter("Super_Soul_" + s.language + ".csv");
-            sw.WriteLine("ID (Numeric)" + sep + "ID (HEX)" + sep + "Name");
-            foreach (IDB_Data i in idb.items)
-            {
-                string txt = m.Find(i.name);
-                sw.WriteLine(i.id.ToString() + sep + ReverseHexString4(i.id.ToString("x4")) + sep + txt);
-            }
-            sw.Close();
-
-            m = MSGStream.Read(s.XENOFolder + "data/msg" + "/proper_noun_costume_name_" + s.language + ".msg");
-            idb = new IDB();
-            idb.Read(s.XENOFolder + "data/system/item/costume_top_item.idb");
-            sw = new StreamWriter("Costume_" + s.language + ".csv");
-            sw.WriteLine("ID (Numeric)" + sep + "ID (HEX)" + sep + "Name");
-            foreach (IDB_Data i in idb.items)
-            {
-                string txt = m.Find(i.name);
-                sw.WriteLine(i.id.ToString() + sep + ReverseHexString4(i.id.ToString("x4")) + sep + txt);
-            }
-            sw.Close();
-
-            m = MSGStream.Read(s.XENOFolder + "data/msg" + "/proper_noun_accessory_name_" + s.language + ".msg");
-            idb = new IDB();
-            idb.Read(s.XENOFolder + "data/system/item/accessory_item.idb");
-            sw = new StreamWriter("Accessory_" + s.language + ".csv");
-            sw.WriteLine("ID (Numeric)" + sep + "ID (HEX)" + sep + "Name");
-            foreach (IDB_Data i in idb.items)
-            {
-                string txt = m.Find(i.name);
-                sw.WriteLine(i.id.ToString() + sep + ReverseHexString4(i.id.ToString("x4")) + sep + txt);
+                sw.WriteLine(c.id.ToString() + "," + ReverseHexString4(c.id.ToString("x4")) + "," + c.shortname + "," + txt);
             }
             sw.Close();
         }
